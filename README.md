8d3cc157e92a790ccc007b939c3424589fc5d3a7
theGUywhoCodss <helmet.guy.programs@gmail.com>
2026-07-24 19:14:14 -0500

    Made dynamic token selecting.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A discord bot made with DPP. Create a "discord_bot.h" file for the bot token in ~/.config/discord_bot
+A discord bot made with DPP. Create a "TOKEN.txt" file for the bot token in ~/.config/discord_bot
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,55 @@
-#include "discord_bot.h"
+#include <cstddef>
 #include <dpp/dpp.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+const int MAX_TOKENS = 10;
+
+// Get the SELECTED token from the ~/.config/discord_bot.TOKEN.txt file.
+// (Include multiple tokens and you will be able to select between them).
+std::string GET_TOKEN() {
+  // Open the text file for reading
+  std::ifstream file(TOKEN_FILE_PATH);
+
+  // Check if the file was opened successfully
+  if (!file.is_open()) {
+    std::cerr << "Error opening the file!\n";
+    return NULL;
+  }
+
+  std::string s;
+  std::string TOKENS[MAX_TOKENS];
+  int tokenCount = 0;
+  // Get ALL Tokens from a file
+  for (int i = 0; i < MAX_TOKENS; i++) {
+    if (std::getline(file, s)) {
+      TOKENS[i] = s;
+      tokenCount++;
+    } else
+      break;
+  }
+  if (!tokenCount) {
+    std::cerr << "No Tokens!";
+    return NULL;
+  } else if (tokenCount == 1) {
+    return TOKENS[0];
+  }
+  // Close the file
+  file.close();
+
+  // Selection UI & return
+  int selectedInt = 0;
+  for (int i = 0; i < tokenCount; i++) {
+    std::cout << i << ". " << TOKENS[i] << "\n";
+  }
+  std::cout << "Select a token!\n";
+  std::cin >> selectedInt;
+  return TOKENS[selectedInt];
+}
 
 int main() {
-  dpp::cluster bot(BOT_TOKEN);
+  dpp::cluster bot(GET_TOKEN());
 
   bot.on_log(dpp::utility::cout_logger());
 
